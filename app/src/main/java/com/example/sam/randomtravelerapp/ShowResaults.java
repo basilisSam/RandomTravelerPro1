eb03e1ac4f103aa27b0d2a955d84dd2cae99c8d6
constantinos <constantine_flds@hotmail.com>
2018-11-03 08:09:25 +0200

    den 3erw an trexoun sigoura tha exei kati lathakia t apoia tha ta lisoume otan to tre3oume dokimazw kati

--- app/src/main/java/com/example/sam/randomtravelerapp/ShowResaults.java
+++ app/src/main/java/com/example/sam/randomtravelerapp/ShowResaults.java
@@ -1,11 +1,25 @@
 package com.example.sam.randomtravelerapp;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class ShowResaults extends AppCompatActivity {
-String Daysfrom,Daysto,Price,From;
+   public String Daysfrom, Daysto, Price, From;
+    public String Resaults="";
+    public String [] data;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +29,57 @@ String Daysfrom,Daysto,Price,From;
         Daysto = intent.getStringExtra("Tday");
         Price = intent.getStringExtra("Price");
         From = intent.getStringExtra("from");
+    }
+
+
+    private class fetchdataApi extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                URL url = new URL("https://api.skypicker.com/flights?fly_from="+From+"&price_to="+Price
+                                                    +"&nights_in_dst_from="+Daysfrom
+                                                        +"&nights_in_dst_to="+Daysto
+                                                           +"&type_flights=lcc");
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("GET");
+                InputStream inputs = con.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(inputs));
+                String line = "";
+                StringBuilder sb= new StringBuilder();
+
+                //for Vodka
+                while((line=br.readLine())!= null){
+                    sb.append(line+"\n");
+                }
+                inputs.close();
+                Resaults=sb.toString();
+
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+            //try parse json data
+            try{
+                JSONArray ja=new JSONArray(Resaults);
+                JSONObject jo=null;
+                data=new String[ja.length()];
+
+
+                for (int i=0;i<=ja.length();i++){
+                    jo=ja.getJSONObject(i);
+                    data[i]="Απο"+" "+jo.getString("cityFrom")+" "+"Για"+" "+jo.getString("cityΤο");
+                }
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+
+            return null;
+        }
     }
 }
